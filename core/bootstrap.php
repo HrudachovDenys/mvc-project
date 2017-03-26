7e1a5875092aba4c5e23f3aef4637c3b5a31094e
Денис Грудачев <dev.lasthero@outlook.com>
2017-03-27 01:51:02 +0300

    test two commit

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -5,4 +5,5 @@ require_once ROOT.DS.'core'.DS.'classes'.DS.'controller.php';
 require_once ROOT.DS.'core'.DS.'classes'.DS.'view.php';
 require_once ROOT.DS.'core'.DS.'classes'.DS.'model.php';
 
+
 Router::run();
