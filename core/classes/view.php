ede8ace092aba5f72a997179011304ddd0d1b831
Денис Грудачев <dev.lasthero@outlook.com>
2017-03-28 02:30:23 +0300

    class view

--- core/classes/view.php
+++ core/classes/view.php
@@ -1,8 +1,61 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+class View
+{
+    private $templatePath = null;
+    private $viewPath;
+    
+    public function __construct($view = null, $template = null)
+    {
+        if($view != null)
+        {
+            $this->viewPath = VIEWS_PATH.DS.$view.'.php';
+        }
+        else
+        {
+            $this->viewPath = VIEWS_PATH.DS.app::getRouter()->getController().'.php';
+        }
+        
+        if($template != null)
+        {
+            $this->setTemplate($template);
+        }
+    }
+    
+    private function setTemplate($template)
+    {
+        $this->templatePath = TEMPLATE_PATH.DS.$template.'.php';
+    }
+    
+    private function renderTemplate()
+    {
+        ob_start();
+        $this->renderDefault();
+        $content = ob_get_clean();
+        
+        if(file_exists($this->templatePath))
+        {
+            include $this->templatePath;
+        }
+    }
+    
+    private function renderDefault()
+    {
+        if(file_exists($this->viewPath))
+        {
+            include $this->viewPath;
+        }
+    }
+    
+    public function render()
+    {
+        if ($this->templatePath != null)
+        {
+            $this->renderTemplate();
+        }
+        else
+        {
+            $this->renderDefault();
+        }
+    }
+}
