527814be0735711fc0470df4af8d7188ad43e50b
Денис Грудачев <dev.lasthero@outlook.com>
2017-03-28 02:21:31 +0300

    Add const VIEWS_PATH and TEMPLATE_PATH

--- web/index.php
+++ web/index.php
@@ -1,9 +1,8 @@
 <?php
 
-define('DIRROOT', 'mvc-project');
 define('DS', DIRECTORY_SEPARATOR);
 define('ROOT', dirname(dirname(__FILE__)));
-define('DEF_CONTROLLER', 'main');
-define('DEF_ACTION', 'index');
+define('VIEWS_PATH', ROOT.DS.'app'.DS.'views');
+define('TEMPLATE_PATH', ROOT.DS.'app'.DS.'templates');
 
 require_once ROOT.DS.'core'.DS.'bootstrap.php';
