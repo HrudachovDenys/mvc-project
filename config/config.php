06d7c9ae164b54e810fe0cc4fd46956eae511da9
Денис Грудачев <dev.lasthero@outlook.com>
2017-03-28 02:34:30 +0300

    add connection DB settings

--- config/config.php
+++ config/config.php
@@ -1,8 +1,10 @@
 <?php
 
-Config::set('site_name', 'mvc-project');
+Config::set('site_name', 'mysite.com');
 
 //default controller
 Config::set('default_controller', 'main');
 //default action
 Config::set('default_action', 'index');
+//db connection
+Config::set('db_conn', include 'db.php');
---
+++ config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'server' => 'loaclhost',
+    'db' => '',
+    'charset' => 'utf-8',
+    'user' => 'root',
+    'pass' => ''
+];
