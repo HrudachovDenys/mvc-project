a61b8499765bbe4d1db73fc1b1dc6a417e54831a
Денис Грудачев <dev.lasthero@outlook.com>
2017-03-28 02:49:29 +0300

    template

---
+++ app/templates/default.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?= app::getRouter()->getAction() ?></title>
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/script.js"></script>
+    </head>
+    <body>
+        ddd
+        <?=$content?>
+    </body>
+</html>
